58e0cb2b32fe4dff938f06bc0ac31742c58ab54f
NguyenDuong21 <thien123111999@gmail.com>
2024-03-07 14:38:05 +0700

    Add code build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   const containerdIp = req.socket.localAddress;
   const containerName = os.hostname();
   res.json({
-    serviceName: "Duongnx v2 v2",
+    serviceName: "Duongnx v2 v2 code build",
     clientIp,
     elbIP,
     containerdIp,
@@ -30,7 +30,7 @@ app.get('/users', (req, res) => {
   const containerName = os.hostname();
   res.json({
     message: "This is from user request",
-    serviceName: "Duongnx v2 v2",
+    serviceName: "Duongnx v2 v2 code build",
     clientIp,
     elbIP,
     containerdIp,
